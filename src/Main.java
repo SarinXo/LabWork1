07c62d023ffc88a4bb33aae1ff0deda62e0434f7
Free-ak <sergzor@list.ru>
2023-02-03 22:54:20 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import Task1.Task1;
+
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        Task1 task_1 = new Task1();
+        task_1.Task_1();
+
+    }
+}
