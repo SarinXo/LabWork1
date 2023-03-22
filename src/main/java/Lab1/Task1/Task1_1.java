3d0407b66c271f84f0bc940eafc699112292b883
udako <Sergzor@list.ru>
2023-03-22 15:38:04 +0300

    Lab2

---
+++ src/main/java/Lab1/Task1/Task1_1.java
@@ -0,0 +1,57 @@
+package Lab1.Task1;
+import jfree__.simpleGui;
+import inputData.inputData;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.text.DecimalFormat;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Task1_1 extends inputData {
+
+    public Map<Float, Long> rowStat;
+    private final JFrame frame = simpleGui.frameDefault("Задание №1", 600, 450);
+
+    private void Table1() {
+        String[] columnname1 = {"Вариационный ряд"};
+        DefaultTableModel TModel1 = new DefaultTableModel(columnname1, 0);
+        for (Float aFloat : row_sort) {
+            Object[] addingRow = {aFloat};
+            TModel1.addRow(addingRow);
+        }
+        simpleGui.scrollDefault(new JTable(TModel1), frame, 140, 380);
+    }
+
+    private void Table2() {
+        String[] columnname2 = {"Элемент", "Частота"};
+        DefaultTableModel TModel2 = new DefaultTableModel(columnname2, 0);
+        Map<Float, Long> map = row_sort.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        //Очень плохой вариант map.entrySet().stream().sorted(Map.Entry.<Float, Long>comparingByKey()).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+        rowStat = new TreeMap<>(map);
+        for (var i : rowStat.entrySet()) {
+            Object[] addingRow = {i.getKey(), i.getValue()};
+            TModel2.addRow(addingRow);
+        }
+        simpleGui.scrollDefault(new JTable(TModel2), frame, 140, 380);
+    }
+
+    private void Table3() {
+        String[] columnname3 = {"Элемент", "Частота", "Процент"};
+        DefaultTableModel TModel3 = new DefaultTableModel(columnname3, 0);
+        for (var i : rowStat.entrySet()) {
+            Object[] addingRow = {new DecimalFormat("#0.0000").format(i.getKey()), new DecimalFormat("#0.0000").format((double) i.getValue()), new DecimalFormat("#0.0000").format((double) i.getValue() / row_sort.size())};
+            TModel3.addRow(addingRow);
+        }
+        simpleGui.scrollDefault(new JTable(TModel3), frame, 200, 380);
+    }
+
+
+    public void Task_1() {
+        Table1();
+        Table2();
+        Table3();
+    }
+}
+
---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import Lab2.Task2_1;
+
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+//        Task1_1 task_1 = new Task1_1();
+//        task_1.Task_1();
+//        Task2 t=new Task2();
+//        t.Task2();
+//        Task3 task_3=new Task3();
+//        task_3.Task3();
+        Task2_1 task=new Task2_1();
+        task.Task2();
+    }
+}
